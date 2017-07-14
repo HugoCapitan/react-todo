c5ab98607adcdd8c0dd39b16ff2ed6892b5aee54
HugoCapitan <hmoravilez@gmail.com>
2017-07-13 20:53:13 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,91 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      actualTodo: {text: '', done: false},
+      todos: []
+    }
+  }
+  update(e) {
+    this.setState({actualTodo: {
+      text: e.target.value,
+      done: this.state.actualTodo.done,
+      index: this.state.todos.length
+    }});
+  }
+  appendTodo(e) {
+    let todos = this.state.todos;
+    todos.push(this.state.actualTodo);
+    this.setState({todos, actualTodo: {
+      text: '',
+      done: false
+    }});
+  }
+  editTodo(editIndex) {
+    this.setState({actualTodo: this.state.todos[editIndex]});
+    this.removeTodo.bind(this, editIndex)();
+  }
+  removeTodo(removeIndex) {
+    let todos = this.state.todos.filter((todo, index) => {
+      return removeIndex !== index;
+    });
+    this.setState({todos});
+  }
+  toggleTodoDone(toggleIndex) {
+    let todos = this.state.todos;
+    todos.map((todo, index) => {
+      if (toggleIndex === index) {
+        todo.done = !todo.done;
+      }
+      return todo;
+    });
+    this.setState({todos});
+    console.log(todos);
+  }
   render() {
+    let todos = this.state.todos;
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div id="app">
+        <TodoInput val={this.state.actualTodo.text} appenMethod={this.appendTodo.bind(this)} changeMethod={this.update.bind(this)}/>
+        { todos.map((todo, index) =>
+          <Todo key={`todo-${index}`}
+                index={index}
+                text={todo.text}
+                done={todo.done}
+                editMethod={this.editTodo.bind(this, index)}
+                removeMethod={this.removeTodo.bind(this, index)}
+                toggleMethod={this.toggleTodoDone.bind(this, index)}/> )}
       </div>
     );
   }
+}
+
+const TodoInput = (props) => (
+  <div id="input">
+    <input value={props.val} type="text" onChange={props.changeMethod}/>
+    <button onClick={props.appenMethod}>Add</button>
+  </div>
+)
+
+TodoInput.propTypes = {
+  appenMethod: React.PropTypes.func.isRequired
+}
+
+const Todo = (props) => (
+  <div className="todo">
+    <span onClick={props.toggleMethod} className={props.done ? 'done' : 'pending'}>
+      {props.index}.- {props.text}
+    </span>
+    <button onClick={props.editMethod}>Edit</button>
+    <button onClick={props.removeMethod}>Remove</button>
+  </div>
+);
+
+Todo.propTypes = {
+  text: React.PropTypes.string
 }
 
 export default App;
